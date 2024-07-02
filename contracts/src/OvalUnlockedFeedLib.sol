c142ed8ae539dc25064b0e57419a9812d5fce983
Reinis Martinsons <reinis@umaproject.org>
2024-07-02 11:36:42 +0000

    wip
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- contracts/src/OvalUnlockedFeedLib.sol
+++ contracts/src/OvalUnlockedFeedLib.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.25;
 
 import {IChainlinkSourceAdapter} from "./IChainlinkSourceAdapter.sol";
+import {Math} from "openzeppelin-contracts/contracts/utils/math/Math.sol";
 import {
     AggregatorV3Interface,
     ChainlinkDataFeedLib
@@ -13,6 +14,8 @@ library OvalUnlockedFeedLib {
     using ChainlinkDataFeedLib for AggregatorV3Interface;
 
     function tryGetSourceOracle(AggregatorV3Interface feed) internal view returns (AggregatorV3Interface) {
+        if (address(feed) == address(0)) return feed;
+
         try IChainlinkSourceAdapter(address(feed)).CHAINLINK_SOURCE() returns (AggregatorV3Interface source) {
             return source;
         } catch {
@@ -29,6 +32,12 @@ library OvalUnlockedFeedLib {
 
         uint8 sourceDecimals = source.decimals();
         uint8 feedDecimals = feed.decimals();
-        return source.getPrice();
+        return convertDecimals(source.getPrice(), sourceDecimals, feedDecimals);
+    }
+
+    function convertDecimals(uint256 answer, uint8 iDecimals, uint8 oDecimals) internal pure returns (uint256) {
+        if (iDecimals == oDecimals) return answer;
+        if (iDecimals < oDecimals) return answer * 10 ** (oDecimals - iDecimals);
+        return answer / 10 ** (iDecimals - oDecimals);
     }
 }
