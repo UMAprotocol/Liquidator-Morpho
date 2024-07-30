968b53fc713b9899519542ee0b36e4f1b8d01fd1
Reinis Martinsons <reinis@umaproject.org>
2024-07-30 11:26:41 +0000

    fix: linter
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- app/src/common/constants_lib.rs
+++ app/src/common/constants_lib.rs
@@ -3,7 +3,8 @@ use ethers::types::U256;
 
 // Oracle price scale. As 1e36 does not fit in a u64 and we cannot call `U256::from` in const context, we have to
 // set it manually here.
-pub const ORACLE_PRICE_SCALE: U256 = U256([0xb34b9f1000000000 as u64, 0xc097ce7bc90715 as u64, 0, 0]);
+pub const ORACLE_PRICE_SCALE: U256 =
+    U256([0xb34b9f1000000000 as u64, 0xc097ce7bc90715 as u64, 0, 0]);
 
 // Liquidation cursor.
 pub const LIQUIDATION_CURSOR: U256 = U256([0.3e18 as u64, 0, 0, 0]);
