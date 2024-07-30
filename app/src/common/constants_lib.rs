3fb2985d9329f9b82563051ef760aa5cf2bac21e
Reinis Martinsons <reinis@umaproject.org>
2024-07-30 09:55:28 +0000

    fix: oracle price scale overflow
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- app/src/common/constants_lib.rs
+++ app/src/common/constants_lib.rs
@@ -1,8 +1,9 @@
 // Selected constants from https://github.com/morpho-org/morpho-blue/blob/main/src/libraries/ConstantsLib.sol
 use ethers::types::U256;
 
-// Oracle price scale.
-pub const ORACLE_PRICE_SCALE: U256 = U256([1e36 as u64, 0, 0, 0]);
+// Oracle price scale. As 1e36 does not fit in a u64 and we cannot call `U256::from` in const context, we have to
+// set it manually here.
+pub const ORACLE_PRICE_SCALE: U256 = U256([0xb34b9f1000000000 as u64, 0xc097ce7bc90715 as u64, 0, 0]);
 
 // Liquidation cursor.
 pub const LIQUIDATION_CURSOR: U256 = U256([0.3e18 as u64, 0, 0, 0]);
